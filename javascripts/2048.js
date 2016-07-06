46273e0c3c56bb66da272b4145c0ad6a74d6b841
Leah <leahnpetersen@gmail.com>
2016-07-06 09:46:08 -0700

    added fuction to show current used divs on board

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -24,24 +24,41 @@ var Game = function() {
 
 }
 
+Game.prototype.boardState = function() {
+  // go through current container objects and make an array of used coordinates ([row, col])
+  var array = [];
+  for (var tile in this.container) {
+    if (tile != undefined) {      
+    array.push([this.container[tile].row, this.container[tile].col]);
+    }
+  }
+  console.log(array + " = BOARDSTATE ARRAY")
+};
+
 Game.prototype.newTile = function () {
  for (var space in this.container) {
   if (this.container[space].col === undefined) {
     var tileQ = $('#' + space)
+    
+
     // get random col and row 
     var rando_col = Math.floor((Math.random() * 4) + 0);
     var rando_row = Math.floor((Math.random() * 4) + 0);
     var val = 2;
-    // update object
-    this.container[space].col = rando_col;
-    this.container[space].row = rando_row;
-    this.container[space].val = val;
+
+    // need to check if div exists with same col/row
+
 
     // update div info
     tileQ.attr('data-row', "r" + rando_row);
     tileQ.attr('data-col', "c" + rando_col);
     tileQ.attr('data-val', val);
     tileQ.text(val);
+    // update object
+    this.container[space].col = rando_col;
+    this.container[space].row = rando_row;
+    this.container[space].val = val;
+
 
     // need to check that something isn't already on this tile
     return
@@ -91,21 +108,21 @@ Game.prototype.buildUpArray = function() {
 
   // shift arrays and update div and object values
   // first step is to just move tiles up as far as they go (end of board or another tile)
-  for (var i = 0; i < 4; i++) {
-    if (array0[0] === 0) {
-      array0.shift();
-    } else if (array0[0] != 0) {
-      // if at top of array, leave there and check the next element
-      if (array0[1] === 0) {
-        array0.shift();
-      } else {
-        if (array0[0][3] === array0[1][3]) {
-          console.log(array0[0][0])
-          console.log(array0[1][0])
-          // write a function to clear div/obj 1 and double val in 0
-        }
-      }
-      continue
+  // for (var i = 0; i < 4; i++) {
+  //   if (array0[0] === 0) {
+  //     array0.shift();
+  //   } else if (array0[0] != 0) {
+  //     // if at top of array, leave there and check the next element
+  //     if (array0[1] === 0) {
+  //       array0.shift();
+  //     } else {
+  //       if (array0[0][3] === array0[1][3]) {
+  //         console.log(array0[0][0])
+  //         console.log(array0[1][0])
+  //         // write a function to clear div/obj 1 and double val in 0
+  //       }
+  //     }
+      // continue
 
 
       // put element at top if there is nothing to combine with yet
@@ -118,9 +135,9 @@ Game.prototype.buildUpArray = function() {
     // tileQ.attr('data-col', "c" + rando_col);
     // tileQ.attr('data-val', val);
     // tileQ.text(val);
-
-    } 
-  }
+    // } 
+    this.newTile();
+  // }
 
 };
 
